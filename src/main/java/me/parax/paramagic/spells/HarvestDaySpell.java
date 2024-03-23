1afedbf1d6f739462c06a6d4014c53d4ec4caff1
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2024-03-23 15:31:17 +0300

    Обновление заклинаний
    Начата разработка посоха тишины.
    Обновлены заклинания:
    1) Цветные сообщения и ивенты обновлены до актуальных версий с помощью Component и AsyncChatEvent
    2) Изменены эффекты в заклинании силы дельфина. Урожай в заклинании сбора урожая теперь выпадает прямо в игрока, а не раскидывается по площади.
    3) Код заклинания сбора урожая был распараллелен и теперь не вызывает подлагивания сервера благодаря переносу основной массы вычислений в асинхронный поток.
    4) В магическую переплавку добавлены некоторые руды и переплавка песка в стекло (список будет расширяться)
    Алгоритм переписан на более оптимальный.

--- src/main/java/me/parax/paramagic/spells/HarvestDaySpell.java
+++ src/main/java/me/parax/paramagic/spells/HarvestDaySpell.java
@@ -1,24 +1,21 @@
 package me.parax.paramagic.spells;
 
-import me.parax.paramagic.utils;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Ageable;
 import org.bukkit.block.data.Directional;
-import org.bukkit.block.data.FaceAttachable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitTask;
-import org.checkerframework.checker.units.qual.A;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HarvestDaySpell implements Listener {
@@ -30,102 +27,115 @@ public class HarvestDaySpell implements Listener {
         this.plugin = plugin;
     }
 
-//    @EventHandler  Тут код на случай если забуду как менять рост блока
-//    public void test(PlayerInteractEvent event) {
-//        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock().getType() == Material.GRASS_BLOCK) {
-//            Block b = event.getClickedBlock();
-//            Block block = b.getWorld().getBlockAt(new Location(b.getWorld(), b.getX(), b.getY() + 1, b.getZ()));
-//            block.setType(Material.WHEAT);
-//            event.getPlayer().sendMessage(block.getType().name());
-//            BlockData bd = block.getBlockData();
-//            Ageable age = (Ageable) bd;
-//            age.setAge(3);
-//            block.setBlockData(age);
-//        }
-//    }
+    private static Component makeText(String text) {
+        return Component.text("[", TextColor.color(255, 168, 0)).append(Component.text(text, TextColor.color(255, 241, 0))).append(Component.text("]", TextColor.color(255, 168, 0)));
+    }
 
     @EventHandler
-    public void harvestDay(AsyncPlayerChatEvent event) {
-        if (event.getMessage().equalsIgnoreCase("О боги земли, да наступит день жатвы, да соберется урожай. Примите мою душу, да будет счастье земле сей")) {
+    public void onChat(AsyncChatEvent event) {
+        if (PlainTextComponentSerializer.plainText().serialize(event.message()).equalsIgnoreCase("О боги земли, да наступит день жатвы, да соберется урожай. Примите мою душу, да будет счастье земле сей")) {
+            event.setCancelled(true);
+            Player player = event.getPlayer();
             server.getScheduler().runTask(plugin, () -> {
-                event.setCancelled(true);
-                Player player = event.getPlayer();
-                player.sendMessage(utils.ct("&7Вы использовали заклинание"));
                 Location loc = player.getLocation();
                 World world = player.getWorld();
                 Block block = world.getBlockAt(loc);
+                player.sendMessage(Component.text("Вы использовали заклинание", TextColor.color(152, 152, 152)));
                 if (player.getTotalExperience() < 2) {
-                    player.sendMessage(utils.ct("&6[&eБоги отказали вам, так как у вас недостаточно души.&6]"));
+                    player.sendMessage(makeText("Боги отказали вам, так как у вас недостаточно души."));
                     return;
                 }
                 if (block.getType() == Material.FARMLAND) {
+                    List<Block> blocks = new ArrayList<>();
+                    List<Block> replant = new ArrayList<>();
+                    List<Block> destroy = new ArrayList<>();
+                    List<ItemStack> items = new ArrayList<>();
                     double x = loc.getX();
                     double y = loc.getY() + 0.2;
                     double z = loc.getZ();
-                    int count = 0;
-                    for (double i = -5; i < 6; ++i) {
-                        for (double j = -5; j < 6; ++j) {
-                            if (player.getTotalExperience() < 2)
-                                return;
-                            block = world.getBlockAt(new Location(world, x + i, y, z + j));
-                            if ((block.getType() == Material.WHEAT || block.getType() == Material.CARROTS || block.getType() == Material.POTATOES) && ((Ageable) block.getBlockData()).getAge() == 7 && player.getTotalExperience() >= 2) {
-                                player.giveExp(-2);
-                                List<ItemStack> drops = (List<ItemStack>) block.getDrops();
-                                for (ItemStack drop : drops) {
-                                    if (drop.getType() == Material.WHEAT_SEEDS || drop.getType() == Material.CARROT || drop.getType() == Material.POTATO) {
-                                        if (drop.getAmount() > 1)
-                                            drop.subtract();
-                                        else
-                                            continue;
+                    for (double i = -5; i < 6; ++i)
+                        for (double j = -5; j < 6; ++j)
+                            blocks.add(world.getBlockAt(new Location(world, x + i, y, z + j)));
+                    int xp = player.getTotalExperience();
+                    int finalXp = xp;
+                    server.getScheduler().runTaskAsynchronously(plugin, () -> {
+                        int exp = finalXp;
+                        for (Block b : blocks) {
+                            if (exp < 2) break;
+                            if (((b.getType() == Material.WHEAT || b.getType() == Material.CARROTS || b.getType() == Material.POTATOES || b.getType() == Material.BEETROOT) && ((Ageable) b.getBlockData()).getAge() == 7) || (b.getType() == Material.BEETROOTS && ((Ageable) b.getBlockData()).getAge() == 3)) {
+                                replant.add(b);
+                                for (ItemStack drop : b.getDrops()) {
+                                    for (ItemStack item : items) {
+                                        if (item.getType() == drop.getType()) {
+                                            if ((drop.getType() == Material.WHEAT_SEEDS || drop.getType() == Material.CARROT || drop.getType() == Material.POTATO || drop.getType() == Material.BEETROOT_SEEDS) && drop.getAmount() > 1)
+                                                    drop.subtract();
+                                            item.add(drop.getAmount());
+                                            break;
+                                        }
                                     }
-                                    world.dropItem(new Location(world, x + i + 0.5, y, z + j + 0.5), drop);
+                                    items.add(drop);
                                 }
-                                ++count;
-                                block.setType(block.getBlockData().getMaterial());
-                            } else if (block.getType() == Material.BEETROOTS && ((Ageable) block.getBlockData()).getAge() == 3 && player.getTotalExperience() >= 2) {
-                                player.giveExp(-2);
-                                List<ItemStack> drops = (List<ItemStack>) block.getDrops();
-                                for (ItemStack drop : drops) {
-                                    if (drop.getType() == Material.BEETROOT_SEEDS) {
-                                        if (drop.getAmount() > 1)
-                                            drop.subtract();
-                                        else
-                                            continue;
-                                    }
-                                    world.dropItem(new Location(world, x + i + 0.5, y, z + j + 0.5), drop);
-                                }
-                                ++count;
-                                block.setType(block.getBlockData().getMaterial());
-                            } else if ((block.getType() == Material.ATTACHED_MELON_STEM || block.getType() == Material.ATTACHED_PUMPKIN_STEM) && block.getBlockData() instanceof Directional && player.getTotalExperience() >= 2) {
-                                Directional directional = (Directional) block.getBlockData();
-                                player.giveExp(-2);
-                                Block block2;
-                                if (directional.getFacing() == BlockFace.EAST)
-                                     block2 = world.getBlockAt(new Location(world, block.getX() + 1, block.getY(), block.getZ()));
-                                else if (directional.getFacing() == BlockFace.WEST)
-                                     block2 = world.getBlockAt(new Location(world, block.getX() - 1, block.getY(), block.getZ()));
-                                else if (directional.getFacing() == BlockFace.NORTH)
-                                    block2 = world.getBlockAt(new Location(world, block.getX(), block.getY(), block.getZ() - 1));
-                                else
-                                    block2 = world.getBlockAt(new Location(world, block.getX(), block.getY(), block.getZ() + 1));
-                                if (block2.getBlockData().getMaterial() == Material.PUMPKIN || block2.getBlockData().getMaterial() == Material.MELON)
-                                    block2.breakNaturally();
+                                exp -= 2;
+                            } else if ((b.getType() == Material.ATTACHED_MELON_STEM || b.getType() == Material.ATTACHED_PUMPKIN_STEM) && b.getBlockData() instanceof Directional) {
+                                destroy.add(b);
+                                exp -= 2;
                             }
                         }
-                    }
-                    if (count > 0)
-                        player.sendMessage(utils.ct("&6[&eБоги земли вас услышали.&6]"));
-                    else {
-                        player.sendMessage(utils.ct("&6[&eБоги земли пришли на ваш зов, но оказались обмануты. В отместку они забрали у вас часть души&6]"));
-                        if (player.getTotalExperience() < 50)
-                            player.setExp(0);
-                        else
-                            player.giveExp(-50);
-                    }
+                        doSpell(player, replant, destroy, items);
+                    });
                 } else {
-                    player.sendMessage(utils.ct("&6[&eБоги земли вас не услышали. Вы должны стоять на пашне для использования этого заклинания&6]"));
+                    event.getPlayer().sendMessage(makeText("Боги земли вас не услышали. Вы должны стоять на пашне для использования этого заклинани"));
                 }
             });
         }
+    }
+
+    private void doSpell(Player player, List<Block> replant, List<Block> destroy, List<ItemStack> items) {
+        server.getScheduler().runTask(plugin, () -> {
+            int count = 0;
+            int xp = player.getTotalExperience();
+            World world = player.getWorld();
+            for (Block b : replant) {
+                ++count;
+                b.setType(b.getBlockData().getMaterial());
+                xp -= 2;
+            }
+            for (Block block : destroy) {
+                Directional directional = (Directional) block.getBlockData();
+                xp -= 2;
+                ++count;
+                Block block2;
+                if (directional.getFacing() == BlockFace.EAST)
+                    block2 = world.getBlockAt(new Location(world, block.getX() + 1, block.getY(), block.getZ()));
+                else if (directional.getFacing() == BlockFace.WEST)
+                    block2 = world.getBlockAt(new Location(world, block.getX() - 1, block.getY(), block.getZ()));
+                else if (directional.getFacing() == BlockFace.NORTH)
+                    block2 = world.getBlockAt(new Location(world, block.getX(), block.getY(), block.getZ() - 1));
+                else
+                    block2 = world.getBlockAt(new Location(world, block.getX(), block.getY(), block.getZ() + 1));
+                if (block2.getBlockData().getMaterial() == Material.PUMPKIN || block2.getBlockData().getMaterial() == Material.MELON)
+                    for (ItemStack drop : block2.getDrops()) {
+                        for (ItemStack item : items) {
+                            if (item.getType() == drop.getType()) {
+                                item.add(drop.getAmount());
+                                break;
+                            }
+                        }
+                        items.add(drop);
+                    }
+            }
+            if (count > 0) {
+                player.sendMessage(makeText("Боги земли вас услышали."));
+                for (ItemStack item : items)
+                    world.dropItem(player.getLocation(), item);
+                player.setTotalExperience(xp);
+            } else {
+                player.sendMessage(makeText("Боги земли пришли на ваш зов, но оказались обмануты. В отместку они забрали у вас часть души"));
+                if (player.getTotalExperience() < 50)
+                    player.setTotalExperience(0);
+                else
+                    player.giveExp(-50);
+            }
+        });
     }
 }
--- src/main/java/me/parax/paramagic/spells/dolphinPowerSpell.java
+++ src/main/java/me/parax/paramagic/spells/dolphinPowerSpell.java
@@ -1,11 +1,14 @@
 package me.parax.paramagic.spells;
 
 
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -20,32 +23,32 @@ public class dolphinPowerSpell implements Listener {
         this.plugin = plugin;
     }
 
+
+    private static Component makeText(String text) {
+        return Component.text("[", TextColor.color(0, 170, 170)).append(Component.text(text, TextColor.color(83, 255, 255))).append(Component.text("]", TextColor.color(0, 170, 170)));
+    }
+
     @EventHandler
-    public void getDolphinPower(AsyncPlayerChatEvent event) {
-        if (event.getMessage().equalsIgnoreCase("Сила океана, возьми мою душу, придай мне силы стать с тобой единым"))
+    public void getDolphinPower(AsyncChatEvent event) {
+        if (PlainTextComponentSerializer.plainText().serialize(event.message()).equalsIgnoreCase("Сила океана, возьми мою душу, придай мне силы стать с тобой единым"))
         {
             event.setCancelled(true);
             Player player = event.getPlayer();
             Location loc = player.getLocation();
             World world = player.getWorld();
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Вы использовали заклинание"));
-            if (world.getBlockAt(loc).getType() != Material.WATER) {
-                String coloredMessage = ChatColor.translateAlternateColorCodes('&', "&3[&bМоре вас не слышит. Коснитесь воды для общения с морем&3]");
-                player.sendMessage(coloredMessage);
+            player.sendMessage(Component.text("Вы использовали заклинание", TextColor.color(152, 152, 152)));
+            if (world.getBlockAt(loc).getType() != Material.WATER && world.getBlockAt(new Location(loc.getWorld(), loc.getX(), loc.getY() + 1, loc.getZ())).getType() != Material.WATER) {
+                player.sendMessage(makeText("Море вас не слышит. Коснитесь воды для общения с морем"));
             } else if (player.getTotalExperience() >= 50) {
-                String coloredMessage = ChatColor.translateAlternateColorCodes('&', "&3[&bМоре услышало вашу просьбу&3]");
                 server.getScheduler().runTask(plugin, () -> {
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.CONDUIT_POWER, 600, 1, false, false));
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.DOLPHINS_GRACE, 600, 1, false, false));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.CONDUIT_POWER, 600, 0, false, false));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.DOLPHINS_GRACE, 600, 2, false, false));
                     player.playSound(player.getLocation(), Sound.ENTITY_ELDER_GUARDIAN_AMBIENT, 1.0f, 1.0f);
-                    player.sendMessage(coloredMessage);
                     player.giveExp(-50);
                 });
+                player.sendMessage(makeText("Море услышало вашу просьбу"));
             } else {
-                String coloredMessage = ChatColor.translateAlternateColorCodes('&', "&3[&bМоре отказало вам в вашей просьбе&3]");
-                server.getScheduler().runTask(plugin, () -> {
-                    player.sendMessage(coloredMessage);
-                });
+                player.sendMessage(makeText("Море отказало вам в вашей просьбе"));
             }
         }
     }
